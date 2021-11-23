c263287ecac7ba4a4d83eecf4731d2baae857eb1
Bryan Sanchez <83999131+BryanSan25@users.noreply.github.com>
2021-11-23 02:26:10 -0500

    Add files via upload

---
+++ clase 4/js/main.js
@@ -0,0 +1,61 @@
+function closeBtn() {
+    document.getElementsByClassName('close-btn')[0].style.display = 'none';
+}
+
+var primerdiv = document.getElementsByClassName('close-btn')[0];
+var segundo = document.createElement('div');
+primerdiv.appendChild(segundo);
+
+function suma() {
+    return n1 + n2
+}
+function resta(n1, n2) {
+    return n1 - n2
+}
+function division(n1, n2) {
+    return n1 / n2
+}
+function multiplicacion(n1, n2) {
+    return n1 * n2
+}
+
+function inye(inye2) {
+    segundo.innerHTML = 1;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
+function inye2(){
+    segundo.innerHTML = 2;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
+function inye3(){
+    segundo.innerHTML = 3;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
+function inye4(){
+    segundo.innerHTML = 4;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
+function inye5(){
+    segundo.innerHTML = 5;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
+function inye6(){
+    segundo.innerHTML = 6;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
+function inye7(){
+    segundo.innerHTML = 7;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
+function inye8(){
+    segundo.innerHTML = 8;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
+function inye9(){
+    segundo.innerHTML = 9;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
+function inye0(){
+    segundo.innerHTML = 0;
+    document.getElementsByClassName('close-btn')[0].style.display = 'block';
+}
